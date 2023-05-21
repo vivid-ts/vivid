b21dcfcee6124133e1a2b423e16f5345ad3dc6a7
Edqe_ <34704796+Edqe14@users.noreply.github.com>
2023-05-21 18:36:14 +0700

    feat: storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,16 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: ['@edqe/eslint-config'],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['@edqe/eslint-config', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    'react-refresh/only-export-components': 'warn'
+  }
 };
